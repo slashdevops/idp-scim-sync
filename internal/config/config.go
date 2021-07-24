614ecc82ac6c0de2dd5a9de3f746e5e3f721139d
Christian González Di Antonio <christiangda@gmail.com>
2021-07-24 14:58:59 +0200

    feat: add config struct and test for this

---
+++ internal/config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type Config struct {
+	Debug bool
+
+	LogLevel  string `mapstructure:"log_level"`
+	LogFormat string `mapstructure:"log_format"`
+
+	ServiceAccountFile string `mapstructure:"service_account_file"`
+	UserEmail          string `mapstructure:"user_email"`
+}
+
+const (
+	// DefaultLogLevel is the default logging level.
+	DefaultLogLevel = "info"
+
+	// DefaultLogFormat is the default format of the logger
+	DefaultLogFormat = "text"
+
+	// DefaultDebug is the default debug status.
+	DefaultDebug = false
+
+	// DefaultGoogleCredentials is the default credentials path
+	DefaultServiceAccountFile = "credentials.json"
+)
+
+// New returns a new Config
+func NewConfig() *Config {
+	return &Config{
+		Debug:              DefaultDebug,
+		LogLevel:           DefaultLogLevel,
+		LogFormat:          DefaultLogFormat,
+		ServiceAccountFile: DefaultServiceAccountFile,
+	}
+}
+
+// toJSON return a json pretty of the config.
+func (c *Config) toJSON() []byte {
+	JSON, err := json.MarshalIndent(c, "", "  ")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	return JSON
+}
---
+++ internal/config/config_test.go
@@ -0,0 +1,66 @@
+package config
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewConfig(t *testing.T) {
+	assert := assert.New(t)
+
+	cfg := NewConfig()
+
+	assert.NotNil(cfg)
+	assert.NotNil(DefaultLogLevel)
+	assert.NotNil(DefaultLogFormat)
+	assert.NotNil(DefaultDebug)
+	assert.NotNil(DefaultServiceAccountFile)
+
+	assert.Equal(cfg.LogLevel, DefaultLogLevel)
+	assert.Equal(cfg.LogFormat, DefaultLogFormat)
+	assert.Equal(cfg.Debug, DefaultDebug)
+	assert.Equal(cfg.ServiceAccountFile, DefaultServiceAccountFile)
+}
+
+func TestConfig_toJSON(t *testing.T) {
+	type fields struct {
+		Debug              bool
+		LogLevel           string
+		LogFormat          string
+		ServiceAccountFile string
+		UserEmail          string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []byte
+	}{
+		{
+			name:   "default",
+			fields: fields{Debug: false, LogLevel: "info", LogFormat: "text", ServiceAccountFile: "default"},
+			want: []byte(`{
+  "Debug": false,
+  "LogLevel": "info",
+  "LogFormat": "text",
+  "ServiceAccountFile": "default",
+  "UserEmail": ""
+}`),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Config{
+				Debug:              tt.fields.Debug,
+				LogLevel:           tt.fields.LogLevel,
+				LogFormat:          tt.fields.LogFormat,
+				ServiceAccountFile: tt.fields.ServiceAccountFile,
+				UserEmail:          tt.fields.UserEmail,
+			}
+			if got := c.toJSON(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Config.toJSON() =\n%s, \n want %s", got, tt.want)
+			}
+		})
+	}
+}
