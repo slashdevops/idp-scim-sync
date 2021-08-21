bd70991625794ef157b21b587241110799a3f02b
Christian González Di Antonio <christiangda@gmail.com>
2021-08-21 21:53:04 +0200

    feat: add funtion to get the differences between members of the idp and the state

--- internal/sync/operations.go
+++ internal/sync/operations.go
@@ -10,6 +10,11 @@ func createSyncState(sgr *StoreGroupsResult, sgmr *StoreGroupsMembersResult, sur
 // groupsDifferences returns the differences between the groups in the
 // this use the Groups Name as the key.
 // SCIM Groups cannot be updated.
+// return 4 objest of GroupsResult
+// create: groups that exist in "state" but not in "idp"
+// update: groups that exist in "idp" and in "state" but attributes changed in idp
+// equal: groups that exist in both "idp" and "state" and their attributes are equal
+// delete: groups that exist in "state" but not in "idp"
 func groupsDifferences(idp, state *GroupsResult) (create *GroupsResult, update *GroupsResult, equal *GroupsResult, delete *GroupsResult) {
 
 	idpGroups := make(map[string]*Group)
@@ -76,6 +81,11 @@ func groupsDifferences(idp, state *GroupsResult) (create *GroupsResult, update *
 
 // usersDifferences returns the differences between the users in the
 // Users Email as the key.
+// return 4 objest of UsersResult
+// create: users that exist in "state" but not in "idp"
+// update: users that exist in "idp" and in "state" but attributes changed in idp
+// equal: users that exist in both "idp" and "state" and their attributes are equal
+// delete: users that exist in "state" but not in "idp"
 func usersDifferences(idp, state *UsersResult) (create *UsersResult, update *UsersResult, equal *UsersResult, delete *UsersResult) {
 
 	idpUsers := make(map[string]*User)
@@ -94,12 +104,11 @@ func usersDifferences(idp, state *UsersResult) (create *UsersResult, update *Use
 		stateUsers[usr.Email] = struct{}{}
 	}
 
-	// new groups and what equal to them
+	// new users and what equal to them
 	for _, usr := range state.Resources {
 		if _, ok := idpUsers[usr.Email]; !ok {
 
-			// Check if the group email changed
-			// Id changed happen when the group delete and create again with the same name and email I guest
+			// Check if the user fields changed
 			if usr.Name.FamilyName != idpUsers[usr.Email].Name.FamilyName ||
 				usr.Name.GivenName != idpUsers[usr.Email].Name.GivenName ||
 				usr.Active != idpUsers[usr.Email].Active ||
@@ -138,6 +147,71 @@ func usersDifferences(idp, state *UsersResult) (create *UsersResult, update *Use
 	delete = &UsersResult{
 		Items:     len(toDelete),
 		Resources: toDelete,
+	}
+
+	return
+}
+
+// groupsMembersDifferences returns the differences between the members in the
+// Users Email as the key.
+// return 4 objest of GroupsMembersResult
+// create: users that exist in "state" but not in "idp"
+// equal: users that exist in both "idp" and "state" and their attributes are equal
+// delete: users that exist in "state" but not in "idp"
+func groupsMembersDifferences(idp, state *GroupsMembersResult) (create *GroupsMembersResult, equal *GroupsMembersResult, delete *GroupsMembersResult) {
+
+	idpMembers := make(map[string]map[string]struct{})
+	stateMembers := make(map[string]map[string]struct{})
+
+	toCreate := make(GroupsMembers)
+	toEqual := make(GroupsMembers)
+	toDelete := make(GroupsMembers)
+
+	for grpName, mbrs := range *idp.Resources {
+		idpMembers[grpName] = make(map[string]struct{})
+		for _, mbr := range mbrs {
+			idpMembers[grpName][mbr.Email] = struct{}{}
+		}
+	}
+
+	for grpName, mbrs := range *state.Resources {
+		stateMembers[grpName] = make(map[string]struct{})
+		for _, mbr := range mbrs {
+			stateMembers[grpName][mbr.Email] = struct{}{}
+		}
+	}
+
+	for grpName, mbrs := range *state.Resources {
+		for _, mbr := range mbrs {
+			if _, ok := idpMembers[grpName][mbr.Email]; !ok {
+				toDelete[grpName] = append(toDelete[grpName], mbr)
+			} else {
+				toEqual[grpName] = append(toEqual[grpName], mbr)
+			}
+		}
+	}
+
+	for grpName, mbrs := range *idp.Resources {
+		for _, mbr := range mbrs {
+			if _, ok := stateMembers[grpName][mbr.Email]; !ok {
+				toCreate[grpName] = append(toCreate[grpName], mbr)
+			}
+		}
+	}
+
+	create = &GroupsMembersResult{
+		Items:     len(toCreate),
+		Resources: &toCreate,
+	}
+
+	equal = &GroupsMembersResult{
+		Items:     len(toEqual),
+		Resources: &toEqual,
+	}
+
+	delete = &GroupsMembersResult{
+		Items:     len(toDelete),
+		Resources: &toDelete,
 	}
 
 	return
