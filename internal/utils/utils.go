434c64339e883ca63b900d42886c862120bed9d7
Christian González Di Antonio <christiangda@gmail.com>
2021-10-05 21:11:52 +0200

    feat: improve the ToJSON function and add unit test

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -7,6 +7,13 @@ import (
 
 // toJSON return a json pretty of the stc
 func ToJSON(stc interface{}) []byte {
+	if stc == nil {
+		return []byte("")
+	}
+	if stc == "" {
+		return []byte("")
+	}
+
 	JSON, err := json.MarshalIndent(stc, "", "  ")
 	if err != nil {
 		log.Fatalf(err.Error())
