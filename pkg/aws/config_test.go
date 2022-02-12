f37e4976be3df2a2afe5da99b1fffe9bdb6b0aca
Christian González Di Antonio <christiangda@gmail.com>
2022-02-12 12:14:35 +0100

    test: add more coverage

--- pkg/aws/config_test.go
+++ pkg/aws/config_test.go
@@ -39,19 +39,19 @@ func TestNewDefaultConf(t *testing.T) {
 		}
 
 		if Expected["AWS_ACCESS_KEY_ID"] != cred.AccessKeyID {
-			t.Errorf("NewDefaultConf() %q != %s, error = %v", Expected["AWS_ACCESS_KEY_ID"], cred.AccessKeyID, err)
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_ACCESS_KEY_ID"], cred.AccessKeyID, err)
 		}
 
 		if Expected["AWS_SECRET_ACCESS_KEY"] != cred.SecretAccessKey {
-			t.Errorf("NewDefaultConf() %q != %s, error = %v", Expected["AWS_SECRET_ACCESS_KEY"], cred.SecretAccessKey, err)
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_SECRET_ACCESS_KEY"], cred.SecretAccessKey, err)
 		}
 
 		if Expected["AWS_SESSION_TOKEN"] != cred.SessionToken {
-			t.Errorf("NewDefaultConf() %q != %s, error = %v", Expected["AWS_SESSION_TOKEN"], cred.SessionToken, err)
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_SESSION_TOKEN"], cred.SessionToken, err)
 		}
 
 		if Expected["AWS_REGION"] != gotCfg.Region {
-			t.Errorf("NewDefaultConf() %q != %s, error = %v", Expected["AWS_REGION"], gotCfg.Region, err)
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_REGION"], gotCfg.Region, err)
 		}
 	})
 
@@ -85,19 +85,19 @@ func TestNewDefaultConf(t *testing.T) {
 		}
 
 		if Expected["AWS_ACCESS_KEY_ID"] != cred.AccessKeyID {
-			t.Errorf("NewDefaultConf() %q != %s, error = %v", Expected["AWS_ACCESS_KEY_ID"], cred.AccessKeyID, err)
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_ACCESS_KEY_ID"], cred.AccessKeyID, err)
 		}
 
 		if Expected["AWS_SECRET_ACCESS_KEY"] != cred.SecretAccessKey {
-			t.Errorf("NewDefaultConf() %q != %s, error = %v", Expected["AWS_SECRET_ACCESS_KEY"], cred.SecretAccessKey, err)
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_SECRET_ACCESS_KEY"], cred.SecretAccessKey, err)
 		}
 
 		if Expected["AWS_SHARED_CREDENTIALS_FILE"] != cred.Source {
-			t.Errorf("NewDefaultConf() %q != %s, error = %v", Expected["AWS_SHARED_CREDENTIALS_FILE"], cred.Source, err)
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_SHARED_CREDENTIALS_FILE"], cred.Source, err)
 		}
 
 		if Expected["AWS_REGION"] != gotCfg.Region {
-			t.Errorf("NewDefaultConf() %q != %s, error = %v", Expected["AWS_REGION"], gotCfg.Region, err)
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_REGION"], gotCfg.Region, err)
 		}
 	})
 
@@ -131,19 +131,114 @@ func TestNewDefaultConf(t *testing.T) {
 		}
 
 		if Expected["AWS_ACCESS_KEY_ID"] != cred.AccessKeyID {
-			t.Errorf("NewDefaultConf() %q != %s, error = %v", Expected["AWS_ACCESS_KEY_ID"], cred.AccessKeyID, err)
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_ACCESS_KEY_ID"], cred.AccessKeyID, err)
 		}
 
 		if Expected["AWS_SECRET_ACCESS_KEY"] != cred.SecretAccessKey {
-			t.Errorf("NewDefaultConf() %q != %s, error = %v", Expected["AWS_SECRET_ACCESS_KEY"], cred.SecretAccessKey, err)
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_SECRET_ACCESS_KEY"], cred.SecretAccessKey, err)
 		}
 
 		if Expected["AWS_SHARED_CREDENTIALS_FILE"] != cred.Source {
-			t.Errorf("NewDefaultConf() %q != %s, error = %v", Expected["AWS_SHARED_CREDENTIALS_FILE"], cred.Source, err)
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_SHARED_CREDENTIALS_FILE"], cred.Source, err)
 		}
 
 		if Expected["AWS_REGION"] != gotCfg.Region {
-			t.Errorf("NewDefaultConf() %q != %s, error = %v", Expected["AWS_REGION"], gotCfg.Region, err)
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_REGION"], gotCfg.Region, err)
+		}
+	})
+
+	t.Run("using credential and config file and profile default", func(t *testing.T) {
+		GotEnvVars := map[string]string{
+			"AWS_SDK_LOAD_CONFIG":         "true",
+			"AWS_SHARED_CREDENTIALS_FILE": "testdata/default/credentials",
+			"AWS_CONFIG_FILE":             "testdata/default/config",
+		}
+
+		Expected := map[string]string{
+			"AWS_ACCESS_KEY_ID":           "AKIAIOSFODNN7EXAMPLE",
+			"AWS_SECRET_ACCESS_KEY":       "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+			"AWS_REGION":                  "us-east-1",
+			"AWS_SHARED_CREDENTIALS_FILE": "EnvConfigCredentials",
+		}
+
+		for key, value := range GotEnvVars {
+			t.Logf("setting env var: %s", key)
+			os.Setenv(key, value)
+		}
+
+		ctx := context.Background()
+		gotCfg, err := NewDefaultConf(ctx)
+		if err != nil {
+			t.Error(err)
+		}
+
+		cred, err := gotCfg.Credentials.Retrieve(ctx)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if Expected["AWS_ACCESS_KEY_ID"] != cred.AccessKeyID {
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_ACCESS_KEY_ID"], cred.AccessKeyID, err)
+		}
+
+		if Expected["AWS_SECRET_ACCESS_KEY"] != cred.SecretAccessKey {
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_SECRET_ACCESS_KEY"], cred.SecretAccessKey, err)
+		}
+
+		if Expected["AWS_SHARED_CREDENTIALS_FILE"] != cred.Source {
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_SHARED_CREDENTIALS_FILE"], cred.Source, err)
+		}
+
+		if Expected["AWS_REGION"] != gotCfg.Region {
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_REGION"], gotCfg.Region, err)
+		}
+	})
+
+	t.Run("using credential and config file and profile slashdevops", func(t *testing.T) {
+		GotEnvVars := map[string]string{
+			"AWS_PROFILE":                 "default",
+			"AWS_SDK_LOAD_CONFIG":         "true",
+			"AWS_SHARED_CREDENTIALS_FILE": "testdata/default/credentials",
+			"AWS_CONFIG_FILE":             "testdata/default/config",
+		}
+
+		Expected := map[string]string{
+			"AWS_ACCESS_KEY_ID":           "AKIAIOSFODNN7EXAMPLE",
+			"AWS_SECRET_ACCESS_KEY":       "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+			"AWS_REGION":                  "us-east-1",
+			"AWS_SHARED_CREDENTIALS_FILE": "SharedConfigCredentials: testdata/default/credentials",
+		}
+
+		for key, value := range GotEnvVars {
+			t.Logf("setting env var: %s", key)
+			os.Setenv(key, value)
+		}
+
+		ctx := context.Background()
+		gotCfg, err := NewDefaultConf(ctx)
+		if err != nil {
+			t.Error(err)
+		}
+
+		cred, err := gotCfg.Credentials.Retrieve(ctx)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if Expected["AWS_ACCESS_KEY_ID"] != cred.AccessKeyID {
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_ACCESS_KEY_ID"], cred.AccessKeyID, err)
+		}
+
+		if Expected["AWS_SECRET_ACCESS_KEY"] != cred.SecretAccessKey {
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_SECRET_ACCESS_KEY"], cred.SecretAccessKey, err)
+		}
+
+		if Expected["AWS_SHARED_CREDENTIALS_FILE"] != cred.Source {
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_SHARED_CREDENTIALS_FILE"], cred.Source, err)
+		}
+
+		if Expected["AWS_REGION"] != gotCfg.Region {
+			t.Errorf("NewDefaultConf() %q != %q, error = %v", Expected["AWS_REGION"], gotCfg.Region, err)
 		}
 	})
 }
