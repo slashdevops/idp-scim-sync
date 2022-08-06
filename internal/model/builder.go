6b64b26bc15c039cae1f3b1feaaa1f83e7c702a3
Christian González Di Antonio <christiangda@gmail.com>
2022-08-07 00:40:06 +0200

    fix: make builders struct private

--- internal/model/builder.go
+++ internal/model/builder.go
@@ -1,74 +1,74 @@
 package model
 
 // UserBuilder is the builder of User entity.
-type UserBuilder struct {
+type userBuilder struct {
 	u *User
 }
 
 // NewUserBuilder creates a new UserBuilder entity.
-func NewUserBuilder() *UserBuilder {
-	return &UserBuilder{
+func NewUserBuilder() *userBuilder {
+	return &userBuilder{
 		u: &User{},
 	}
 }
 
 // WithIPID sets the IPID field of the User entity.
-func (b *UserBuilder) WithIPID(ipid string) *UserBuilder {
+func (b *userBuilder) WithIPID(ipid string) *userBuilder {
 	b.u.IPID = ipid
 	return b
 }
 
 // WithSCIMID sets the SCIMID field of the User entity.
-func (b *UserBuilder) WithSCIMID(scimid string) *UserBuilder {
+func (b *userBuilder) WithSCIMID(scimid string) *userBuilder {
 	b.u.SCIMID = scimid
 	return b
 }
 
 // WithFamilyName sets the FamilyName field of the User entity.
-func (b *UserBuilder) WithFamilyName(familyName string) *UserBuilder {
+func (b *userBuilder) WithFamilyName(familyName string) *userBuilder {
 	b.u.Name.FamilyName = familyName
 	return b
 }
 
 // WithGivenName sets the GivenName field of the User entity.
-func (b *UserBuilder) WithGivenName(givenName string) *UserBuilder {
+func (b *userBuilder) WithGivenName(givenName string) *userBuilder {
 	b.u.Name.GivenName = givenName
 	return b
 }
 
 // WithDisplayName sets the DisplayName field of the User entity.
-func (b *UserBuilder) WithDisplayName(displayName string) *UserBuilder {
+func (b *userBuilder) WithDisplayName(displayName string) *userBuilder {
 	b.u.DisplayName = displayName
 	return b
 }
 
 // WithActive sets the Active field of the User entity.
-func (b *UserBuilder) WithActive(active bool) *UserBuilder {
+func (b *userBuilder) WithActive(active bool) *userBuilder {
 	b.u.Active = active
 	return b
 }
 
 // WithEmail sets the Email field of the User entity.
-func (b *UserBuilder) WithEmail(email string) *UserBuilder {
+func (b *userBuilder) WithEmail(email string) *userBuilder {
 	b.u.Email = email
 	return b
 }
 
 // Build returns the User entity.
-func (b *UserBuilder) Build() *User {
+func (b *userBuilder) Build() *User {
 	u := b.u
 	u.SetHashCode()
 	return u
 }
 
 // UsersResultBuilder is used to build a UsersResult entity and ensure the calculated hash code and items.
-type UsersResultBuilder struct {
+type usersResultBuilder struct {
 	ur *UsersResult
 }
 
 // NewUsersResultBuilder creates a new UsersResultBuilder entity.
-func NewUsersResultBuilder() *UsersResultBuilder {
-	return &UsersResultBuilder{
+func NewUsersResultBuilder() *usersResultBuilder {
+	return &usersResultBuilder{
 		ur: &UsersResult{
 			Resources: make([]*User, 0),
 		},
@@ -76,19 +76,19 @@ func NewUsersResultBuilder() *UsersResultBuilder {
 }
 
 // WithResources sets the Resources field of the UsersResult entity.
-func (b *UsersResultBuilder) WithResources(resources []*User) *UsersResultBuilder {
+func (b *usersResultBuilder) WithResources(resources []*User) *usersResultBuilder {
 	b.ur.Resources = resources
 	return b
 }
 
 // WithResource add the resource to a Resources field of the UsersResult entity.
-func (b *UsersResultBuilder) WithResource(resource *User) *UsersResultBuilder {
+func (b *usersResultBuilder) WithResource(resource *User) *usersResultBuilder {
 	b.ur.Resources = append(b.ur.Resources, resource)
 	return b
 }
 
 // Build returns the UserResult entity.
-func (b *UsersResultBuilder) Build() *UsersResult {
+func (b *usersResultBuilder) Build() *UsersResult {
 	ur := b.ur
 	ur.Items = len(ur.Resources)
 	ur.SetHashCode()
@@ -96,56 +96,56 @@ func (b *UsersResultBuilder) Build() *UsersResult {
 }
 
 // GroupBuilder is used to build a Group entity and ensure the calculated hash code is set.
-type GroupBuilder struct {
+type groupBuilder struct {
 	g *Group
 }
 
 // NewGroupBuilder creates a new GroupBuilder entity.
-func NewGroupBuilder() *GroupBuilder {
-	return &GroupBuilder{
+func NewGroupBuilder() *groupBuilder {
+	return &groupBuilder{
 		g: &Group{},
 	}
 }
 
 // WithIPID sets the IPID field of the Group entity.
-func (b *GroupBuilder) WithIPID(ipid string) *GroupBuilder {
+func (b *groupBuilder) WithIPID(ipid string) *groupBuilder {
 	b.g.IPID = ipid
 	return b
 }
 
 // WithSCIMID sets the SCIMID field of the Group entity.
-func (b *GroupBuilder) WithSCIMID(scimid string) *GroupBuilder {
+func (b *groupBuilder) WithSCIMID(scimid string) *groupBuilder {
 	b.g.SCIMID = scimid
 	return b
 }
 
 // WithName sets the Name field of the Group entity.
-func (b *GroupBuilder) WithName(name string) *GroupBuilder {
+func (b *groupBuilder) WithName(name string) *groupBuilder {
 	b.g.Name = name
 	return b
 }
 
 // WithEmail sets the Email field of the Group entity.
-func (b *GroupBuilder) WithEmail(email string) *GroupBuilder {
+func (b *groupBuilder) WithEmail(email string) *groupBuilder {
 	b.g.Email = email
 	return b
 }
 
 // Build returns the Group entity.
-func (b *GroupBuilder) Build() *Group {
+func (b *groupBuilder) Build() *Group {
 	g := b.g
 	g.SetHashCode()
 	return g
 }
 
 // GroupsResultBuilder is used to build a GroupsResult entity and ensure the calculated hash code and items is set.
-type GroupsResultBuilder struct {
+type groupsResultBuilder struct {
 	gr *GroupsResult
 }
 
 // NewGroupsResultBuilder creates a new GroupsResultBuilder entity.
-func NewGroupsResultBuilder() *GroupsResultBuilder {
-	return &GroupsResultBuilder{
+func NewGroupsResultBuilder() *groupsResultBuilder {
+	return &groupsResultBuilder{
 		gr: &GroupsResult{
 			Resources: make([]*Group, 0),
 		},
@@ -153,19 +153,19 @@ func NewGroupsResultBuilder() *GroupsResultBuilder {
 }
 
 // WithResources sets the Resources field of the GroupsResult entity.
-func (b *GroupsResultBuilder) WithResources(resources []*Group) *GroupsResultBuilder {
+func (b *groupsResultBuilder) WithResources(resources []*Group) *groupsResultBuilder {
 	b.gr.Resources = resources
 	return b
 }
 
 // WithResource add the resource to a Resources field of the GroupsResult entity.
-func (b *GroupsResultBuilder) WithResource(resource *Group) *GroupsResultBuilder {
+func (b *groupsResultBuilder) WithResource(resource *Group) *groupsResultBuilder {
 	b.gr.Resources = append(b.gr.Resources, resource)
 	return b
 }
 
 // Build returns the GroupsResult entity.
-func (b *GroupsResultBuilder) Build() *GroupsResult {
+func (b *groupsResultBuilder) Build() *GroupsResult {
 	gr := b.gr
 	gr.Items = len(gr.Resources)
 	gr.SetHashCode()
@@ -173,56 +173,56 @@ func (b *GroupsResultBuilder) Build() *GroupsResult {
 }
 
 // MemberBuilder is used to build a Member entity and ensure the calculated hash code is set.
-type MemberBuilder struct {
+type memberBuilder struct {
 	m *Member
 }
 
 // NewMemberBuilder creates a new MemberBuilder entity.
-func NewMemberBuilder() *MemberBuilder {
-	return &MemberBuilder{
+func NewMemberBuilder() *memberBuilder {
+	return &memberBuilder{
 		m: &Member{},
 	}
 }
 
 // WithIPID sets the IPID field of the Member entity.
-func (b *MemberBuilder) WithIPID(ipid string) *MemberBuilder {
+func (b *memberBuilder) WithIPID(ipid string) *memberBuilder {
 	b.m.IPID = ipid
 	return b
 }
 
 // WithSCIMID sets the SCIMID field of the Member entity.
-func (b *MemberBuilder) WithSCIMID(scimid string) *MemberBuilder {
+func (b *memberBuilder) WithSCIMID(scimid string) *memberBuilder {
 	b.m.SCIMID = scimid
 	return b
 }
 
 // WithEmail sets the Email field of the Member entity.
-func (b *MemberBuilder) WithEmail(email string) *MemberBuilder {
+func (b *memberBuilder) WithEmail(email string) *memberBuilder {
 	b.m.Email = email
 	return b
 }
 
 // WithStatus sets the Status field of the Member entity.
-func (b *MemberBuilder) WithStatus(status string) *MemberBuilder {
+func (b *memberBuilder) WithStatus(status string) *memberBuilder {
 	b.m.Status = status
 	return b
 }
 
 // Build returns the Member entity.
-func (b *MemberBuilder) Build() *Member {
+func (b *memberBuilder) Build() *Member {
 	m := b.m
 	m.SetHashCode()
 	return m
 }
 
 // MembersResultBuilder is used to build a MembersResult entity and ensure the calculated hash code and items is set.
-type MembersResultBuilder struct {
+type membersResultBuilder struct {
 	mr *MembersResult
 }
 
 // NewMembersResultBuilder creates a new MembersResultBuilder entity.
-func NewMembersResultBuilder() *MembersResultBuilder {
-	return &MembersResultBuilder{
+func NewMembersResultBuilder() *membersResultBuilder {
+	return &membersResultBuilder{
 		mr: &MembersResult{
 			Resources: make([]*Member, 0),
 		},
@@ -230,19 +230,19 @@ func NewMembersResultBuilder() *MembersResultBuilder {
 }
 
 // WithResources sets the Resources field of the MembersResult entity.
-func (b *MembersResultBuilder) WithResources(resources []*Member) *MembersResultBuilder {
+func (b *membersResultBuilder) WithResources(resources []*Member) *membersResultBuilder {
 	b.mr.Resources = resources
 	return b
 }
 
 // WithResource add the resource to a Resources field of the MembersResult entity.
-func (b *MembersResultBuilder) WithResource(resource *Member) *MembersResultBuilder {
+func (b *membersResultBuilder) WithResource(resource *Member) *membersResultBuilder {
 	b.mr.Resources = append(b.mr.Resources, resource)
 	return b
 }
 
 // Build returns the MembersResult entity.
-func (b *MembersResultBuilder) Build() *MembersResult {
+func (b *membersResultBuilder) Build() *MembersResult {
 	mr := b.mr
 	mr.Items = len(mr.Resources)
 	mr.SetHashCode()
@@ -250,13 +250,13 @@ func (b *MembersResultBuilder) Build() *MembersResult {
 }
 
 // GroupMembersBuilder is used to build a GroupMembers entity and ensure the calculated hash code is set.
-type GroupMembersBuilder struct {
+type groupMembersBuilder struct {
 	gm *GroupMembers
 }
 
 // NewGroupMembersBuilder creates a new GroupMembersBuilder entity.
-func NewGroupMembersBuilder() *GroupMembersBuilder {
-	return &GroupMembersBuilder{
+func NewGroupMembersBuilder() *groupMembersBuilder {
+	return &groupMembersBuilder{
 		gm: &GroupMembers{
 			Resources: make([]*Member, 0),
 		},
@@ -264,25 +264,25 @@ func NewGroupMembersBuilder() *GroupMembersBuilder {
 }
 
 // WithGroup sets the Group field of the GroupMembers entity.
-func (b *GroupMembersBuilder) WithGroup(group *Group) *GroupMembersBuilder {
+func (b *groupMembersBuilder) WithGroup(group *Group) *groupMembersBuilder {
 	b.gm.Group = group
 	return b
 }
 
 // WithResources sets the Resources field of the GroupMembers entity.
-func (b *GroupMembersBuilder) WithResources(resources []*Member) *GroupMembersBuilder {
+func (b *groupMembersBuilder) WithResources(resources []*Member) *groupMembersBuilder {
 	b.gm.Resources = resources
 	return b
 }
 
 // WithResource add the resource to a Resources field of the GroupMembers entity.
-func (b *GroupMembersBuilder) WithResource(resource *Member) *GroupMembersBuilder {
+func (b *groupMembersBuilder) WithResource(resource *Member) *groupMembersBuilder {
 	b.gm.Resources = append(b.gm.Resources, resource)
 	return b
 }
 
 // Build returns the GroupMembers entity.
-func (b *GroupMembersBuilder) Build() *GroupMembers {
+func (b *groupMembersBuilder) Build() *GroupMembers {
 	gm := b.gm
 	gm.Items = len(gm.Resources)
 	gm.SetHashCode()
@@ -290,13 +290,13 @@ func (b *GroupMembersBuilder) Build() *GroupMembers {
 }
 
 // GroupsMembersResultBuilder is used to build a GroupsMembersResult entity and ensure the calculated hash code and items is set.
-type GroupsMembersResultBuilder struct {
+type groupsMembersResultBuilder struct {
 	gmr *GroupsMembersResult
 }
 
 // NewGroupsMembersResultBuilder creates a new GroupsMembersResultBuilder entity.
-func NewGroupsMembersResultBuilder() *GroupsMembersResultBuilder {
-	return &GroupsMembersResultBuilder{
+func NewGroupsMembersResultBuilder() *groupsMembersResultBuilder {
+	return &groupsMembersResultBuilder{
 		gmr: &GroupsMembersResult{
 			Resources: make([]*GroupMembers, 0),
 		},
@@ -304,19 +304,19 @@ func NewGroupsMembersResultBuilder() *GroupsMembersResultBuilder {
 }
 
 // WithResources sets the Resources field of the GroupsMembersResult entity.
-func (b *GroupsMembersResultBuilder) WithResources(resources []*GroupMembers) *GroupsMembersResultBuilder {
+func (b *groupsMembersResultBuilder) WithResources(resources []*GroupMembers) *groupsMembersResultBuilder {
 	b.gmr.Resources = resources
 	return b
 }
 
 // WithResource add the resource to a Resources field of the GroupsMembersResult entity.
-func (b *GroupsMembersResultBuilder) WithResource(resource *GroupMembers) *GroupsMembersResultBuilder {
+func (b *groupsMembersResultBuilder) WithResource(resource *GroupMembers) *groupsMembersResultBuilder {
 	b.gmr.Resources = append(b.gmr.Resources, resource)
 	return b
 }
 
 // Build returns the GroupsMembersResult entity.
-func (b *GroupsMembersResultBuilder) Build() *GroupsMembersResult {
+func (b *groupsMembersResultBuilder) Build() *GroupsMembersResult {
 	gmr := b.gmr
 	gmr.Items = len(gmr.Resources)
 	gmr.SetHashCode()
