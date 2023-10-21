1cd3e75d8225b87568010ff7d1a93613459ccd0a
Christian González Di Antonio <christiangda@gmail.com>
2023-10-21 17:51:46 +0200

    test: add test cases

--- internal/deepcopy/deepcopy.go
+++ internal/deepcopy/deepcopy.go
@@ -4,9 +4,18 @@ package deepcopy
 // The new slice will have the same length and capacity as the given slice.
 // and the values will be copied.
 func SliceOfPointers[T any](s []*T) []*T {
+	if s == nil {
+		return nil
+	}
+
 	newSlice := make([]*T, 0, len(s))
 
 	for _, v := range s {
+		if v == nil {
+			newSlice = append(newSlice, nil)
+			continue
+		}
+
 		// Create a new struct to hold the copied data
 		newStruct := new(T)
 		*newStruct = *v
