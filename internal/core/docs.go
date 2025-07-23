5cbe2072139caa73fb3dbfa6263916a60a7b748a
Christian González Di Antonio <christian.gonzalez@aizon.ai>
2025-07-23 11:55:52 +0200

    fix: package documentation

---
+++ internal/core/docs.go
@@ -0,0 +1,3 @@
+// Package core provides core functionalities for the application.
+// It includes utilities for deep copying data structures, handling SCIM APIs, and managing user identities.
+package core
